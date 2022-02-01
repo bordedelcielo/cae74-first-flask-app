bb787dd28b5bc9057106dd85e1d00093260a2df6
bordedelcielo <christopher.thrutchley@gmail.com>
2022-01-31 19:57:20 -0600

    first commit for pokemon-api

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ class Config():
     }
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
