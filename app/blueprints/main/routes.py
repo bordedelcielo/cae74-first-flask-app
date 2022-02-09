deb31f31ec85d5eba6b6c1285649aa9ccf8b2966
bordedelcielo <christopher.thrutchley@gmail.com>
2022-02-08 21:38:13 -0600

    changed cursor.execute to db.session.execute

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -148,7 +148,8 @@ def pokemon():
 @main.route('/delete/<id>', methods = ['GET', 'POST'])
 def delete(id):
     session_id = session["_user_id"]
-    cursor.execute(f"DELETE FROM association where pokemon_id = '{id}' and user_id = '{session_id}'")
+    db.session.execute(f"DELETE FROM association where pokemon_id = '{id}' and user_id = '{session_id}'")
+    db.session.commit()
     cursor.execute(f"SELECT * FROM pokemon INNER JOIN association ON association.pokemon_id = pokemon.the_pokemon_id WHERE user_id = '{session_id}';")
     data = cursor.fetchall()
 
