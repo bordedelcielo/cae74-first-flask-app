8c4deb13bce55df2d9ffe074c49cf7734eaa0500
bordedelcielo <christopher.thrutchley@gmail.com>
2022-02-11 16:34:16 -0600

    displays pokemon owned by user on arena page

--- app/blueprints/battle/routes.py
+++ app/blueprints/battle/routes.py
@@ -8,7 +8,18 @@ cursor = con.cursor()
 @battle.route('/arena', methods = ['GET', 'POST'])
 def arena():
     id = session["_user_id"]
-    cursor.execute(f"SELECT first_name FROM public.user WHERE id <> '{id}'")
+    cursor.execute(f"SELECT first_name, id FROM public.user WHERE id <> '{id}'")
     data = cursor.fetchall()
-    print(data)
-    return render_template('arena.html.j2', data=data)
+    output_dictionary = {}
+    for row in data:
+        cursor.execute(f"SELECT name, sprite FROM pokemon INNER JOIN association ON association.pokemon_id = pokemon.the_pokemon_id WHERE user_id = '{row[1]}';")
+        poke_data = cursor.fetchall()
+        working_dictionary = {row[1]: poke_data}
+        # print(working_dictionary)
+        output_dictionary.update(working_dictionary)
+    # print(data)
+    # print(data[0][1])
+    # print(output_dictionary)
+    print(output_dictionary[1][0][0])
+    # print(working_dictionary)
+    return render_template('arena.html.j2', data=data, dictionary = output_dictionary)
