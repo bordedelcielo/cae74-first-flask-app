c37f62b282d62d7b10ce617e52d412f193eace3e
bordedelcielo <christopher.thrutchley@gmail.com>
2022-02-11 14:30:06 -0600

    table created to display user data

--- app/blueprints/battle/routes.py
+++ app/blueprints/battle/routes.py
@@ -1,6 +1,14 @@
 from .import bp as battle
-from flask import render_template
+from flask import render_template, session
+
+from app.secrets import con
+
+cursor = con.cursor()
 
 @battle.route('/arena', methods = ['GET', 'POST'])
 def arena():
-    return render_template('arena.html.j2')
+    id = session["_user_id"]
+    cursor.execute(f"SELECT first_name FROM public.user WHERE id <> '{id}'")
+    data = cursor.fetchall()
+    print(data)
+    return render_template('arena.html.j2', data=data)
