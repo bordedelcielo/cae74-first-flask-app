ca51eb2683e86ad885b3c74398339346b90cf9cc
bordedelcielo <christopher.thrutchley@gmail.com>
2022-02-15 20:33:50 -0600

    arena displays other users and pokemon

--- app/blueprints/battle/routes.py
+++ app/blueprints/battle/routes.py
@@ -9,21 +9,19 @@ cursor = con.cursor()
 @battle.route('/arena', methods = ['GET', 'POST'])
 def arena():
     id = session["_user_id"]
-    cursor.execute(f"SELECT first_name, id, wins FROM public.user WHERE id <> '{id}'")
-    data = cursor.fetchall()
+    test = User.query.all()
+    print(test)
     output_dictionary = {}
-    for row in data:
-        cursor.execute(f"SELECT name, sprite FROM pokemon INNER JOIN association ON association.pokemon_id = pokemon.the_pokemon_id WHERE user_id = '{row[1]}';")
-        poke_data = cursor.fetchall()
-        working_dictionary = {row[1]: poke_data}
-        # print(working_dictionary)
-        output_dictionary.update(working_dictionary)
-    # print(data)
-    # print(data[0][1])
-    # print(output_dictionary)
-    # print(output_dictionary[1][0][0])
-    # print(working_dictionary)
-    return render_template('arena.html.j2', data=data, dictionary = output_dictionary)
+    user_list = []
+    for item in test:
+        if item.id != int(id):
+            user_list.append(item)
+            user = User.query.get(item.id)
+            children_list = [element.sprite for element in user.children.all()]
+            working_dictionary = {item.first_name: children_list}
+            output_dictionary.update(working_dictionary)
+
+    return render_template('arena.html.j2', data = test, output_dictionary = output_dictionary, user_list = user_list)
 
 @battle.route('/challenge/<id_for_use>', methods = ['GET', 'POST'])
 def challenge(id_for_use):
